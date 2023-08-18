60193114d79c8d95cd16f3bf04d684a38b6bc757
BTPNS\20220637 <20220637@mail.btpnsyariah.com>
2023-08-18 15:38:42 +0700

    Add Task 2 Python

--- Task 2 Python/main.py
+++ Task 2 Python/main.py
@@ -1,90 +1,38 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 
-
-# In[2]:
-
-
 emp_df = pd.read_csv("employees.csv")
-emp_df.head()
-
-
-# In[49]:
-
 
 ts_df = pd.read_csv("timesheets.csv")
-ts_df.head()
-
-
-# In[50]:
-
 
 ts_df['checkin'] = pd.to_datetime(ts_df['checkin'].fillna('09:00:00'))
 ts_df['checkout'] = pd.to_datetime(ts_df['checkout'].fillna('17:00:00'))
-ts_df['date'] = pd.to_datetime(ts_df['checkout'])
-
-
-# In[51]:
-
+ts_df['date'] = pd.to_datetime(ts_df['date'])
 
 ts_df['hours_difference'] = (ts_df['checkout'] - ts_df['checkin']).dt.total_seconds() / (60*60)
 
-
-# In[52]:
-
-
+ts_df["year"] = ts_df['date'].dt.year
+ts_df["month"] = ts_df['date'].dt.month
 merged_df = pd.merge(ts_df, emp_df, left_on='employee_id', right_on='employe_id', how='left')
 
+agg_data = merged_df.groupby(['year', 'month', 'branch_id', 'employee_id']).agg({
+    'salary': 'first',            # Get the first salary (assuming salaries are the same for the same employee)
+    'hours_difference': 'sum'          # Sum of hour_worked
+}).reset_index()
 
-# In[32]:
+agg_data = merged_df.groupby(['year', 'month', 'branch_id', 'employee_id']).agg({
+    'salary': 'first',            # Get the first salary (assuming salaries are the same for the same employee)
+    'hours_difference': 'sum'          # Sum of hour_worked
+}).reset_index()
 
+# Group by year, month, and branch_id again to calculate the sum of salaries and total hour worked
+final_agg_data = agg_data.groupby(['year', 'month', 'branch_id']).agg({
+    'salary': 'sum',               # Sum of unique salaries
+    'hours_difference': 'sum'           # Sum of hour_worked
+}).reset_index()
 
-df_salary = merged_df.copy()
-df_salary = df_salary[["timesheet_id","employee_id","date","salary"]]
+final_agg_data.rename(columns={'salary': 'salary_total'}, inplace=True)
 
+final_agg_data["salary_per_hour"] = final_agg_data["salary_total"]/final_agg_data["hours_difference"]
 
-# In[53]:
-
-
-merged_df["year"] = merged_df['date'].dt.year
-merged_df["month"] = merged_df['date'].dt.month
-
-
-# In[54]:
-
-
-merged_df
-
-
-# In[59]:
-
-
-z = merged_df.groupby(['year', 'month', 'branch_id'])
-
-
-# In[64]:
-
-
-z.head()
-
-
-# In[65]:
-
-
-f = {
-        'employee_id' : 'nunique'
-}
-v1 = z.agg(f)
-v2 = z.agg(lambda x: x.drop_duplicates('employee_id', keep='first').salary.sum())
-
-
-# In[66]:
-
-
-v2
+print(final_agg_data)
 
