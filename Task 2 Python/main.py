0a2ab40ad512cb16de81eb75c5d6da9b4958d348
BTPNS\20220637 <20220637@mail.btpnsyariah.com>
2023-08-18 16:06:53 +0700

    Final Result

--- Task 2 Python/main.py
+++ Task 2 Python/main.py
@@ -22,21 +22,49 @@ def insert_to_table(host, user, password, database, df):
     conn.commit()
     cursor.close()
 
+def get_max_date_from_destination(host, user, password, database):
+    conn = mysql.connector.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database
+    )
+    cursor = conn.cursor()
+
+    query = "SELECT MAX(date) AS id FROM timesheets"
+    cursor.execute(query)
+
+    # Fetch the result
+    result = cursor.fetchone()
+    max_date = result[0]  # The maximum timesheet_id
+    cursor.close()
+    return max_date
 
 def main():
+    # Read File
     emp_df = pd.read_csv("employees.csv")
     ts_df = pd.read_csv("timesheets.csv")
 
+    # Make it as datetime type
     ts_df['checkin'] = pd.to_datetime(ts_df['checkin'].fillna('09:00:00'))
     ts_df['checkout'] = pd.to_datetime(ts_df['checkout'].fillna('17:00:00'))
     ts_df['date'] = pd.to_datetime(ts_df['date'])
 
+    # GET ONLY NEW DATA
+    max_date = get_max_date_from_destination("localhost","root","","mekari")
+    ts_df = ts_df[ts_df['date'] > max_date]
+
+    # Get hours difference
     ts_df['hours_difference'] = (ts_df['checkout'] - ts_df['checkin']).dt.total_seconds() / (60*60)
 
+    # Add year and month column
     ts_df["year"] = ts_df['date'].dt.year
     ts_df["month"] = ts_df['date'].dt.month
+
+    # Merge timesheet with employee
     merged_df = pd.merge(ts_df, emp_df, left_on='employee_id', right_on='employe_id', how='left')
 
+    ### Get unique sum salary for each year, month, branch_id
     agg_data = merged_df.groupby(['year', 'month', 'branch_id', 'employee_id']).agg({
         'salary': 'first',            # Get the first salary (assuming salaries are the same for the same employee)
         'hours_difference': 'sum'          # Sum of hour_worked
@@ -55,11 +83,16 @@ def main():
 
     final_agg_data.rename(columns={'salary': 'salary_total'}, inplace=True)
 
+    # Get Salary Per Hour
     final_agg_data["salary_per_hour"] = final_agg_data["salary_total"]/final_agg_data["hours_difference"]
 
-    print(final_agg_data)
+    # print(final_agg_data)
 
     insert_to_table("localhost","root","","mekari",final_agg_data)
+
+    # To make sure this is working, we need to also insert new timesheet data (ts_df) to timesheets table
+    # insert_to_timesheet_table() 
+    # Not implemented yet
 
 
 if __name__ == "__main__":
