386a545783836fa15ce14183b1af2aa4fb58d71b
BTPNS\20220637 <20220637@mail.btpnsyariah.com>
2023-08-18 15:48:53 +0700

    Make it more readable

--- Task 2 Python/main.py
+++ Task 2 Python/main.py
@@ -1,63 +1,66 @@
 import pandas as pd
 import mysql.connector
 
-emp_df = pd.read_csv("employees.csv")
 
-ts_df = pd.read_csv("timesheets.csv")
+def insert_to_table(host, user, password, database, df):
+    # Establish the connection
+    conn = mysql.connector.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database
+    )
 
-ts_df['checkin'] = pd.to_datetime(ts_df['checkin'].fillna('09:00:00'))
-ts_df['checkout'] = pd.to_datetime(ts_df['checkout'].fillna('17:00:00'))
-ts_df['date'] = pd.to_datetime(ts_df['date'])
+    table_name = 'salary_per_hours'
 
-ts_df['hours_difference'] = (ts_df['checkout'] - ts_df['checkin']).dt.total_seconds() / (60*60)
+    sql = f"INSERT INTO {table_name} (year, month, branch_id, total_salary, total_hours, salary_per_hour) VALUES (%s, %s, %s, %s, %s, %s)"
 
-ts_df["year"] = ts_df['date'].dt.year
-ts_df["month"] = ts_df['date'].dt.month
-merged_df = pd.merge(ts_df, emp_df, left_on='employee_id', right_on='employe_id', how='left')
+    values = [tuple(row) for row in df.values]
 
-agg_data = merged_df.groupby(['year', 'month', 'branch_id', 'employee_id']).agg({
-    'salary': 'first',            # Get the first salary (assuming salaries are the same for the same employee)
-    'hours_difference': 'sum'          # Sum of hour_worked
-}).reset_index()
+    cursor = conn.cursor()
+    cursor.executemany(sql, values)
+    conn.commit()
+    cursor.close()
 
-agg_data = merged_df.groupby(['year', 'month', 'branch_id', 'employee_id']).agg({
-    'salary': 'first',            # Get the first salary (assuming salaries are the same for the same employee)
-    'hours_difference': 'sum'          # Sum of hour_worked
-}).reset_index()
 
-# Group by year, month, and branch_id again to calculate the sum of salaries and total hour worked
-final_agg_data = agg_data.groupby(['year', 'month', 'branch_id']).agg({
-    'salary': 'sum',               # Sum of unique salaries
-    'hours_difference': 'sum'           # Sum of hour_worked
-}).reset_index()
+def main():
+    emp_df = pd.read_csv("employees.csv")
+    ts_df = pd.read_csv("timesheets.csv")
 
-final_agg_data.rename(columns={'salary': 'salary_total'}, inplace=True)
+    ts_df['checkin'] = pd.to_datetime(ts_df['checkin'].fillna('09:00:00'))
+    ts_df['checkout'] = pd.to_datetime(ts_df['checkout'].fillna('17:00:00'))
+    ts_df['date'] = pd.to_datetime(ts_df['date'])
 
-final_agg_data["salary_per_hour"] = final_agg_data["salary_total"]/final_agg_data["hours_difference"]
+    ts_df['hours_difference'] = (ts_df['checkout'] - ts_df['checkin']).dt.total_seconds() / (60*60)
 
-print(final_agg_data)
+    ts_df["year"] = ts_df['date'].dt.year
+    ts_df["month"] = ts_df['date'].dt.month
+    merged_df = pd.merge(ts_df, emp_df, left_on='employee_id', right_on='employe_id', how='left')
 
-host = 'your_host'
-user = 'your_user'
-password = 'your_password'
-database = 'your_database'
+    agg_data = merged_df.groupby(['year', 'month', 'branch_id', 'employee_id']).agg({
+        'salary': 'first',            # Get the first salary (assuming salaries are the same for the same employee)
+        'hours_difference': 'sum'          # Sum of hour_worked
+    }).reset_index()
 
-# Establish the connection
-conn = mysql.connector.connect(
-    host=host,
-    user=user,
-    password=password,
-    database=database
-)
+    agg_data = merged_df.groupby(['year', 'month', 'branch_id', 'employee_id']).agg({
+        'salary': 'first',            # Get the first salary (assuming salaries are the same for the same employee)
+        'hours_difference': 'sum'          # Sum of hour_worked
+    }).reset_index()
 
-table_name = 'salary_per_hours'
-columns = ', '.join(final_agg_data.columns)
+    # Group by year, month, and branch_id again to calculate the sum of salaries and total hour worked
+    final_agg_data = agg_data.groupby(['year', 'month', 'branch_id']).agg({
+        'salary': 'sum',               # Sum of unique salaries
+        'hours_difference': 'sum'           # Sum of hour_worked
+    }).reset_index()
 
-sql = f"INSERT INTO {table_name} (year, month, branch_id, total_salary, total_hours, salary_per_hour) VALUES (%s, %s, %s, %s, %s, %s)"
+    final_agg_data.rename(columns={'salary': 'salary_total'}, inplace=True)
 
-values = [tuple(row) for row in final_agg_data.values]
+    final_agg_data["salary_per_hour"] = final_agg_data["salary_total"]/final_agg_data["hours_difference"]
 
-cursor = conn.cursor()
-cursor.executemany(sql, values)
-conn.commit()
-cursor.close()
+    print(final_agg_data)
+
+    insert_to_table("localhost","root","","mekari",final_agg_data)
+
+
+if __name__ == "__main__":
+    main()
